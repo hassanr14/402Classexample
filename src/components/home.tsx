fa61673f1e8a9c214b19f168df9c85822dba2ae1
hassanr14 <hassanr14@coventry.ac.uk>
2024-03-18 21:34:23 +0000

    initial

---
+++ src/components/home.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Home() {
+
+
+    return(
+
+      <section className="hero is-danger is-fullheight">
+      <div className="hero-body">
+        <div className="">
+          <p className="title">
+            Fullheight hero
+          </p>
+          <p className="subtitle">
+            Fullheight subtitle
+          </p>
+        </div>
+      </div>
+    </section>
+
+    )
+}
